63021e5a38245a7c8aea7e9f795f427e2f15e297
GiuliBruno14 <128742172+GiuliBruno14@users.noreply.github.com>
2024-08-22 17:45:47 -0300

    Display inquilino y requerimiento de campos

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -1,17 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PROYECTO_BRUNO_SOAZO.Models;
 
 public class Inquilino
 {
-    
+
+    [Display(Name = "CÓDIGO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public int Id { get; set; }
+    [Display(Name = "NOMBRE")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Nombre { get; set; }
+    [Display(Name = "APELLIDO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Apellido { get; set; }
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [Display(Name = "E-MAIL")]
     public string? Email { get; set; }
-    
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [Display(Name = "DNI")]
     public string? Dni { get; set; }
+    [Display(Name = "TELEFONO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Telefono { get; set; }
+    [Display(Name = "DOMICILIO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Domicilio { get; set; }
+    [Display(Name = "CIUDAD")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Ciudad { get; set; }
     public string? RequestId { get; set; }
-   // public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    // public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -5,24 +5,30 @@ namespace PROYECTO_BRUNO_SOAZO.Models;
 public class Propietario
 {
     [Display(Name = "CÓDIGO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public int Id { get; set; }
     [Display(Name = "NOMBRE")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Nombre { get; set; }
     [Display(Name = "APELLIDO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Apellido { get; set; }
+    [Required(ErrorMessage = "Campo obligatorio")]
     [Display(Name = "E-MAIL")]
     public string? Email { get; set; }
-     [Required(ErrorMessage = "Campo obligatorio")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     [Display(Name = "DNI")]
     public string? Dni { get; set; }
     [Display(Name = "TELEFONO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Telefono { get; set; }
     [Display(Name = "DOMICILIO")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Domicilio { get; set; }
     [Display(Name = "CIUDAD")]
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? Ciudad { get; set; }
     public string? RequestId { get; set; }
    // public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
     
 }
